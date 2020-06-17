355aec960771442216282df74f7415c266ac94da
asketonim <anton.dubik33@gmail.com>
2020-06-17 19:18:08 +0300

    Compose main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Main.module.css';
+
+const Main = () => (
+  <main className={classes.Main}>
+    there will be posts
+  </main>
+);
+
+export default Main;
