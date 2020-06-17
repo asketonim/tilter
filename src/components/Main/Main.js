382049662f97ee72dbac93a32f14d5ca1824cb1f
asketonim <anton.dubik33@gmail.com>
2020-06-17 19:30:38 +0300

    Create Post component body

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Post from '../Post/Post';
+
 import classes from './Main.module.css';
 
 const Main = () => (
   <main className={classes.Main}>
-    there will be posts
+    <Post />
   </main>
 );
 
---
+++ src/components/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Post.module.css';
+
+const Post = () => (
+  <article className={classes.Post}>
+    q
+  </article>
+);
+
+export default Post;
