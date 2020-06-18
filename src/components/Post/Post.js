44c29de0329a70c41c3d8745caa9c95bedd7b612
asketonim <anton.dubik33@gmail.com>
2020-06-18 23:55:59 +0300

    Create ProfilePhoto(.svg) component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import ProfilePhoto from './ProfilePhoto';
+
 import classes from './Post.module.css';
 
 const Post = () => (
   <article className={classes.Post}>
-    q
+    <div className={classes.Row}>
+      <div>
+        <ProfilePhoto />
+      </div>
+      <div>
+        Text
+      </div>
+    </div>
+    <div className={classes.Row}>
+      <div>
+        Likes
+      </div>
+      <div>
+        Edit
+      </div>
+    </div>
   </article>
 );
 
