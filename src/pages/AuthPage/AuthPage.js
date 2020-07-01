b86a3b8ca5056cdc17f12bebcd83c5f10243542b
asketonim <anton.dubik33@gmail.com>
2020-07-01 14:32:57 +0300

    Create AuthPage
    - add username and password forms
    - add simple styling
    - attach state and changeHandler to forms

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,21 +1,66 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 
 import classes from './AuthPage.module.css';
 
-const AuthPage = () => (
-  <div className={classes.AuthPage}>
-    <div className={classes.Label}>
-      Authorization
+const AuthPage = () => {
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  });
+
+  const formChangeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className={classes.AuthPage}>
+      <div className={classes.Label}>
+        <h2>tilter</h2>
+      </div>
+      <div className={classes.FormHolder}>
+
+        <div className={classes.Form}>
+          <label>
+            Username:
+            <br />
+            <input
+              placeholder="Enter username"
+              id="username"
+              type="text"
+              name="username"
+              onChange={formChangeHandler}
+              required
+            />
+          </label>
+        </div>
+
+        <div className={classes.Form}>
+          <label>
+            Password:
+            <br />
+            <input
+              placeholder="Enter password"
+              id="password"
+              type="password"
+              name="password"
+              onChange={formChangeHandler}
+              required
+            />
+          </label>
+        </div>
+
+      </div>
+      <div className={classes.ButtonHolder}>
+        <button className={classes.btn} type="button">
+          Sign in
+        </button>
+        <button className={classes.btn} type="button">
+          Sign up
+        </button>
+      </div>
     </div>
-    <div className={classes.ButtonHolder}>
-      <button className={classes.btn} type="button">
-        Sign in
-      </button>
-      <button className={classes.btn} type="button">
-        Sign up
-      </button>
-    </div>
-  </div>
-);
+  )
+};
 
 export default AuthPage;
